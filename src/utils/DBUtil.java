8e5d4a126084f976747b046a9334ba3b4f28a1f1
happy <happy@HAPPY-11700>
2022-05-08 09:54:32 +0900

    getAllMessagesを追加

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME ="message_boaed";
+    private static final String PERSISTENCE_UNIT_NAME ="message_board";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
